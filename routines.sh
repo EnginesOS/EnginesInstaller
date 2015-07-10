b39fa551129de28cbb4cce7b2260d816bb6ae997
James Vodanovich <james@jvodan.com>
2015-07-10 15:39:18 +1000

    remove deps on /opt/engines

--- routines.sh
+++ routines.sh
@@ -2,23 +2,6 @@
 RUBY_VER=2.2.2
 
 
-function complete_install {
-
-create_services > /var/log/engines/install_startup
-
-/opt/engines/bin/containers_startup.sh >> /var/log/engines/install_startup
-
-echo "System startup"
-/opt/engines/bin/engines_startup.sh
-
-rm /opt/engines/.complete_install
-touch /opt/engines/.installed
-
-hostname=`hostname`
-
-
-echo "Congratulations Engines OS is now installed please go to http://${hostname}:88/"
-}
 
 function configure_git {
 
