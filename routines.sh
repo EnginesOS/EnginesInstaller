c2f157dd608e88b6a621b05a71fc512c38e64d96
James Vodanovich <james@jvodan.com>
2015-07-26 12:44:36 +1000

    fix fall out from removing excessive chown -R /var/log/engines/

--- routines.sh
+++ routines.sh
@@ -217,9 +217,9 @@ echo "Creating Management Service Dirs"
 	mkdir -p /opt/engines/run/service_manager/
 	chown -R 21000 /opt/engines/run/service_manager/
 	chown -R 21000 /home/engines/deployment/deployed/
-	chown 21000 /var/lib/engines  /var/log/engines/containers /var/log/engines/
+	chown 21000 /var/lib/engines  /var/log/engines/containers /var/log/engines/ /var/log/engines/services/ /var/log/engines/containers/
 	
-	chown -R 21000 ~engines/  /var/lib/engines/mgmt/public/system/
+	chown -R 21000 ~engines/  /var/lib/engines/mgmt/public/system/ /var/log/engines/services/mgmt 
 	mkdir -p /opt/engines/run/system/flags/
 	chown -R 21000 /opt/engines/run/system/
 	
