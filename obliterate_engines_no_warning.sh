bfe4f98d642dcd21a1601bd9ff803b5121a2eb2f
James Vodanovich <james@jvodan.com>
2015-08-11 16:51:43 +1000

    add are you sure? prompt

--- obliterate_engines_no_warning.sh
+++ obliterate_engines_no_warning.sh
@@ -1,7 +1,14 @@
 #!/bin/sh
 
+read -p "Are you sure? " -n 1 -r
+echo    # (optional) move to a new line
+if [[ ! $REPLY =~ ^[Yy]$ ]]
+then
+    exit 1
+fi
+
 echo "15 seconds until destruction with no visual countdown starting now"
-echo "Docker and ALL docker Images will be Deleted -k to keep"
+echo "Docker and ALL docker Images will be Deleted -k to keep docker images"
 echo "Ctl-C now if this is not want you want"
 sleep 20
 keep=0
