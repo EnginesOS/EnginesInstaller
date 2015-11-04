2bd4d8377d7311cd53ac644b813abc43ef3c6a6e
James L Vodanovich <james@jvodan.com>
2015-11-04 10:38:53 +0000

    dbg

--- routines.sh
+++ routines.sh
@@ -443,6 +443,7 @@ function create_mgmt_script_key {
 	echo "command=\"/opt/engines/bin/${script_name}.sh\",no-port-forwarding,no-X11-forwarding,no-agent-forwarding,no-pty  $pubkey " >  ~/.ssh/_${script_name}_authorized_keys
 	cat ~/.ssh/_${script_name}_authorized_keys >> ~/.ssh/authorized_keys.system
 	cp ~/.ssh/mgmt/${script_name} ~/.ssh/mgmt/${script_name}.pub /opt/engines/etc/ssh/keys/services/mgmt
+	echo cp ~/.ssh/mgmt/${script_name} ~/.ssh/mgmt/${script_name}.pub /opt/engines/etc/ssh/keys/services/mgmt
 }
 
 function setup_mgmt_keys {
