dd9b2dd1116be5797ed02b5165f2559290767662
James L Vodanovich <james@jvodan.com>
2016-01-26 22:43:02 +0000

    correct paths

--- routines/root_user/service_definitions.sh
+++ routines/root_user/service_definitions.sh
@@ -19,7 +19,7 @@ mkdir -p database/sql/mysql
 mkdir -p filesystem/local/filesystem
 
 cd ../
-to_map="cron backup avahi mongo pqsql mysql filesystem syslog"
+to_map="cron backup avahi mongo pgsql mysql filesystem syslog"
 
 for service in $to_map
 	do
@@ -35,7 +35,7 @@ for service in $to_map
 		service_def=`find providers/ -name ${service}.yaml`
 		echo service_def  $service_def 
 		cp $service_def mapping/database/sql/mysql
-		cp $service_def mapping/database/pqsql/mysql
+		cp $service_def mapping/database/sql/pgsql
 		cp $service_def mapping/filesystem/local/filesystem
     done
     
