70dd5a77df73e258ef63773fc882abbc313a96b1
James L Vodanovich <james@jvodan.com>
2016-09-14 11:27:27 +1000

    base os update

--- routines/engines_user/crontab.sh
+++ routines/engines_user/crontab.sh
@@ -1,9 +1,6 @@
 function setup_engines_crontab {
 		
 	echo "Setup engines cron tab"
-#echo "*/10 * * * * /opt/engines/bin/engines.sh engine check_and_act all >>/opt/engines/logs/engines/restarts.log 
-#*/10 * * * * /opt/engines/bin/engines.sh  service  check_and_act all >>/opt/engines/logs/services/restarts.log
-#* 3 * * * /opt/engines/bin/engines_system_update_status.sh" >/tmp/ct
 
 crontab -u engines ${top}/install_source/etc/crontab
 
