c15130ddbf04761ce2125cf0b2c4ce92d7f4912b
James L Vodanovich <james@jvodan.com>
2017-02-03 15:37:35 +1100

    dhcpd

--- routines/root_user/os_routines.sh
+++ routines/root_user/os_routines.sh
@@ -41,6 +41,6 @@ apt-get install -y apt-transport-https    linux-image-extra-$(uname -r) lvm2 thi
   echo "Adding systemd"
   cp  ${top}/install_source/lib/systemd/system/engines.service /lib/systemd/system/engines.service
   systemctl daemon-reload 
-  service engines enable
+  systemctl enable engines
  
   }
