a475c7568d429be2e6226593ed43b58ecbe97893
James L Vodanovich <james@jvodan.com>
2017-05-15 15:26:39 +1000

    only start bare minimum

--- routines/engines_user/services_first_start.sh
+++ routines/engines_user/services_first_start.sh
@@ -97,49 +97,50 @@ docker  start system  >& /dev/null
 #echo "enter key to continue"
 #read next
 sleep 5
+
 /opt/engines/bin/engines service dns restart >& /dev/null
 echo "System services restarted"
 
 echo "Downloading Syslog image"
 	docker pull engines/syslog:$release >>/tmp/engines_install.log
-	if test $? -ne 0
-	 then
-	  echo pull of engines/syslog:$release failed check your network
-	    install_failed
-	fi
-echo "Starting Syslog" 
-	 /opt/engines/bin/engines service syslog create >>/tmp/engines_install.log
-	 
+ 	if test $? -ne 0
+ 	 then
+ 	  echo pull of engines/syslog:$release failed check your network
+ 	    install_failed
+ 	fi
+#echo "Starting Syslog" 
+#	 /opt/engines/bin/engines service syslog create >>/tmp/engines_install.log
+#	 
 echo "Downloading Avahi image"
 	docker pull engines/avahi:$release >>/tmp/engines_install.log
-	if test $? -ne 0
-	 then
-	  echo pull of engines/avahi:$release failed check your network
-	    install_failed
-	fi
-echo "Starting Avahi"
-	 /opt/engines/bin/engines service avahi create >>/tmp/engines_install.log
+ 	if test $? -ne 0
+ 	 then
+ 	  echo pull of engines/avahi:$release failed check your network
+ 	    install_failed
+ 	fi
+#echo "Starting Avahi"
+#	 /opt/engines/bin/engines service avahi create >>/tmp/engines_install.log
 	  
 echo "Downloading Cert Auth image"
 	 docker pull engines/certs:$release >>/tmp/engines_install.log
-	if test $? -ne 0
-	 then
-	  echo pull of engines/certs:$release failed check your network
-	    install_failed
-	fi
-echo "Starting Cert Auth"
-	/opt/engines/bin/engines service  cert_auth create >>/tmp/engines_install.log
-	
+ 	if test $? -ne 0
+ 	 then
+ 	  echo pull of engines/certs:$release failed check your network
+ 	    install_failed
+ 	fi
+#echo "Starting Cert Auth"
+#	/opt/engines/bin/engines service  cert_auth create >>/tmp/engines_install.log
+#	
 echo "Downloading MySQL image"
 	 docker pull engines/mysql:$release >>/tmp/engines_install.log
-	if test $? -ne 0
-	 then
-	  echo pull of engines/mysql:$release failed check your network
-	    install_failed
-	fi
-echo "Starting MySQL"	 
-	 /opt/engines/bin/engines service  mysql_server create >>/tmp/engines_install.log
-	  
+ 	if test $? -ne 0
+ 	 then
+ 	  echo pull of engines/mysql:$release failed check your network
+ 	    install_failed
+ 	fi
+#echo "Starting MySQL"	 
+#	 /opt/engines/bin/engines service  mysql_server create >>/tmp/engines_install.log
+#	  
 echo "Downloading First Run Wizard  image"
 	  docker pull engines/firstrun:$release >>/tmp/engines_install.log
 	if test $? -ne 0
@@ -163,44 +164,44 @@ echo "Starting Auth"
 	 
 echo "Downloading Web Router image"
 	  docker pull engines/nginx:$release >>/tmp/engines_install.log
-	if test $? -ne 0
-	 then
-	  echo pull of engines/nginx:$release failed check your network
-	    install_failed
-	fi
-echo "Starting Web Router"
-	 /opt/engines/bin/engines service nginx create >>/tmp/engines_install.log
+ 	if test $? -ne 0
+ 	 then
+ 	  echo pull of engines/nginx:$release failed check your network
+ 	    install_failed
+ 	fi
+#echo "Starting Web Router"
+#	 /opt/engines/bin/engines service nginx create >>/tmp/engines_install.log
 	 
 echo "Downloading Backup image"
 	 docker pull engines/backup:$release >>/tmp/engines_install.log
-	if test $? -ne 0
-	 then
-	  echo pull of engines/backup:$release failed check your network
-	    install_failed
-	fi
-echo "Starting Backup"
-	 /opt/engines/bin/engines service backup create >>/tmp/engines_install.log
+ 	if test $? -ne 0
+ 	 then
+ 	  echo pull of engines/backup:$release failed check your network
+ 	    install_failed
+ 	fi
+#echo "Starting Backup"
+#	 /opt/engines/bin/engines service backup create >>/tmp/engines_install.log
 	 
 echo "Downloading Cron image"
 	 docker pull engines/cron:$release >>/tmp/engines_install.log
-	if test $? -ne 0
-	 then
-	  echo pull of engines/cron:$release failed check your network
-	    install_failed
-	fi
- echo "Starting Cron"
-	 /opt/engines/bin/engines service cron create >>/tmp/engines_install.log
+ 	if test $? -ne 0
+ 	 then
+ 	  echo pull of engines/cron:$release failed check your network
+ 	    install_failed
+ 	fi
+# echo "Starting Cron"
+#	 /opt/engines/bin/engines service cron create >>/tmp/engines_install.log
  	 
 
 echo "Downloading SMTP image"
 	 docker pull engines/smtp:$release >>/tmp/engines_install.log
-	if test $? -ne 0
-	 then
-	  echo pull of engines/smtp:$release failed check your network
-	    install_failed
-	fi
- echo "Starting SMTP"
-	 /opt/engines/bin/engines service smtp create >>/tmp/engines_install.log
+ 	if test $? -ne 0
+ 	 then
+ 	  echo pull of engines/smtp:$release failed check your network
+ 	    install_failed
+ 	fi
+# echo "Starting SMTP"
+#	 /opt/engines/bin/engines service smtp create >>/tmp/engines_install.log
   
   echo "Downloading mgmt image"
  	 
@@ -208,13 +209,13 @@ docker pull engines/mgmt:$release >>/tmp/engines_install.log
 	  
 echo "Downloading FTP image"
 	docker pull engines/ftp:$release >>/tmp/engines_install.log
-	if test $? -ne 0
-	 then
-	  echo pull of engines/ftp:$release failed check your network
-	    install_failed
-	fi
-echo "Starting FTP"
-	 /opt/engines/bin/engines service  ftp create >>/tmp/engines_install.log
+ 	if test $? -ne 0
+ 	 then
+ 	  echo pull of engines/ftp:$release failed check your network
+ 	    install_failed
+ 	fi
+#echo "Starting FTP"
+#	 /opt/engines/bin/engines service  ftp create >>/tmp/engines_install.log
  	
 	
 echo "Downloading Volmanager image"
@@ -227,20 +228,20 @@ echo "Downloading Volmanager image"
 	
 	docker pull engines/volbuilder:$release
 	
-echo "Starting Volmanager Service"
-	//opt/engines/bin/engines service volmanager create >>/tmp/engines_install.log
+#echo "Starting Volmanager Service"
+#	//opt/engines/bin/engines service volmanager create >>/tmp/engines_install.log
 	
  docker pull engines/fsconfigurator:$release
 
 
 echo "Downloading Volmanager image"
 	docker pull engines/logrotate:$release
-echo "Starting Log Rotate Service"	
-	/opt/engines/bin/engines service log_rotate create
+#echo "Starting Log Rotate Service"	
+#	/opt/engines/bin/engines service log_rotate create
 	
-echo "Started System Services"
-	# /opt/engines/bin/engines containers  check_and_act  >>/tmp/engines_install.log &
-	 
-	# docker pull engines/mgmt:$release
+#echo "Started System Services"
+#	# /opt/engines/bin/engines containers  check_and_act  >>/tmp/engines_install.log &
+#	 
+#	# docker pull engines/mgmt:$release
 	
 }
