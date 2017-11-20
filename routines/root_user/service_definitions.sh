95dd117b10d44f1530b8bf2f6d5a4844553306e7
James L Vodanovich <james@jvodan.com>
2017-11-20 16:18:13 +1100

    check out the right branch

--- routines/root_user/service_definitions.sh
+++ routines/root_user/service_definitions.sh
@@ -13,7 +13,13 @@ function setup_service_definitions {
 	  mkdir -p services/providers
 	  cd services/providers
 	  git clone https://github.com/EnginesServices/SumoLogic
+	  cd SumoLogic
+	  git checkout `cat /opt/engines/release`
+	  cd ..
 	  git clone https://github.com/EnginesServices/EnginesSystem
+	  cd EnginesSystem
+	   git checkout `cat /opt/engines/release`
+	   cd ..
 	#git clone https://github.com/EnginesOS/ServiceDefinitions services
 	#cd services
 	#git checkout `cat /opt/engines/release`
