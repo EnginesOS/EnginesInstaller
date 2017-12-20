e3ee3c2ba89cb39572a5618967d1130034a515b4
James L Vodanovich <james@jvodan.com>
2017-12-21 09:30:34 +1100

    f

--- routines/root_user/init_ssl_cert.sh
+++ routines/root_user/init_ssl_cert.sh
@@ -1,6 +1,6 @@
 function copy_install_ssl_cert {
 	#echo "Installing installation ssl cert"
-	mkdir -p /var/lib/engines/services/certs/public/certs/ /var/lib/engines/services/certs/public/keys/
+	mkdir -p /var/lib/engines/services/certs/store/public/certs/ /var/lib/engines/services/certs/store/public/keys/
 	chown -R 22022 /var/lib/engines//services/certs/
-		
+   		
 }
