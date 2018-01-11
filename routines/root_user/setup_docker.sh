e7e12d6facccfb42e57ff76aefbcec6f30dfcbbf
James L Vodanovich <james@jvodan.com>
2018-01-11 12:11:30 +1100

    tidy detection of whether package is installed

--- routines/root_user/setup_docker.sh
+++ routines/root_user/setup_docker.sh
@@ -26,7 +26,7 @@ function install_docker_components {
 
 	  for package in $packages
  	    do
- 	 	   dpkg -l $package
+ 	 	  apt list $package  |grep $package 
  	 	     if test $? -ne 0
  	  		   then
  	 		      packages_to_install="$packages_to_install $package"
