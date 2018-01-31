94f34ae9efecff46295cd46250d64428d3cbc3e9
James L Vodanovich <james@jvodan.com>
2018-01-31 13:08:00 +1100

    use apt-cache and not apt

--- routines/root_user/setup_docker.sh
+++ routines/root_user/setup_docker.sh
@@ -26,7 +26,7 @@ function install_docker_components {
 
 	  for package in $packages
  	    do
- 	 	  apt list $package  |grep $package 
+ 	 	  apt-cache show $package  |grep $package 
  	 	     if test $? -ne 0
  	  		   then
  	 		      packages_to_install="$packages_to_install $package"
