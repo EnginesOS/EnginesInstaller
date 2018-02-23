bc214c5a6289b88a80ae7ea9c6b3cfcf4103057f
James L Vodanovich <james@jvodan.com>
2018-02-23 14:02:19 +1100

    check first run is running with engines with in http mode

--- complete_install.sh
+++ complete_install.sh
@@ -44,11 +44,10 @@ cp /etc/os-release /opt/engines/etc/os-release-host
  
 /opt/engines/bin/engines service firstrun wait_for_startup 325
 # FixME Test other services as well
-if ! test running = `/opt/engines/bin/engines service firstrun state`
+if ! test running = `CONTROL_HTTP=y /opt/engines/bin/engines service firstrun state`
  then
    echo "INSTALLATION FAILED"
    echo "First Run is not running"
- 
    exit
   fi
 
